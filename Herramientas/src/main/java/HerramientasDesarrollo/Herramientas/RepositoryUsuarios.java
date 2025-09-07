9e4e070aab85395f1a62b38b94a6a30f57c32660
Marcksss <145176782+Marcksss@users.noreply.github.com>
2025-09-07 09:59:35 -0500

    Delete Herramientas directory

--- Herramientas/src/main/java/HerramientasDesarrollo/Herramientas/RepositoryUsuarios.java
+++
@@ -1,6 +0,0 @@
-package HerramientasDesarrollo.Herramientas;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryUsuarios extends JpaRepository<Usuarios, Integer> {
-}
--- Herramientas/src/main/java/HerramientasDesarrollo/Herramientas/UsuariosRepository.java
+++
@@ -1,7 +0,0 @@
-package HerramientasDesarrollo.Herramientas;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
-    Usuarios findByNombre(String nombre);  // <--- AGREGAR ESTE MÉTODO
-}
