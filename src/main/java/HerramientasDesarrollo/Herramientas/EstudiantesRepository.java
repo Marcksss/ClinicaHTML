93e4d8765ae4a7f8bc6e2564ba106126766bf7c3
Marcksss <quilluya2002@gmail.com>
2025-10-17 22:30:16 -0500

    se agrega clase

---
+++ src/main/java/HerramientasDesarrollo/Herramientas/EstudiantesRepository.java
@@ -0,0 +1,7 @@
+package HerramientasDesarrollo.Herramientas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstudiantesRepository extends JpaRepository<Estudiantes, Integer> {
+
+}
