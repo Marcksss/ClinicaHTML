b3449851cd2f85e2d506a7f473ebdd70a3d34eee
Marcksss <quilluya2002@gmail.com>
2025-11-03 22:31:16 -0500

    se agrega clase

---
+++ src/main/java/HerramientasDesarrollo/Herramientas/RepositoryReporteErrorClinica.java
@@ -0,0 +1,7 @@
+package HerramientasDesarrollo.Herramientas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryReporteErrorClinica extends JpaRepository<ReporteErrorClinica, Integer> {
+
+}
